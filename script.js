9dabb1abb6b2b3a50e0c24e99447d4738bca5a1a
barseegiah <giahseymehnbarseegiah@gmail.com>
2024-06-07 01:46:51 +0100

    Completed styling

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
-
-//Load DOM before script
+// Load DOM before script
 document.addEventListener('DOMContentLoaded', () => {
-    
-    //Get html element
-    // const searchbtn = document.getElementById('searchBtn').disabled = true;
+            
+    // Get html element
     const location = document.getElementById('location');
     const temperature = document.getElementById('temperature');
     const description = document.getElementById('description');
     const form = document.querySelector('form');
 
-    function weather(){
+    function weather() {
 
-        //Store input value
+        // Store input value
         let userInput = document.querySelector('#searchInput').value;
         
-        //store api key into a variable
+        // Store API key into a variable
         const apiKEY = '9f72a35202d3629e1833a38dcd371b5b';
         
-        //Condition for input field
-        if(userInput != ''){
+        // Condition for input field
+        if (userInput != '') {
 
-        document.getElementById('message').innerHTML = 'Location';
-        //Fetch api
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKEY)
-        .then(function (response) {
-            return response.json()
-        })
-        .then(function (data){
+            document.getElementById('message').innerHTML = 'Location';
+            // Fetch API
+            fetch('https://api.openweathermap.org/data/2.5/weather?q=' + userInput + '&appid=' + apiKEY)
+                .then(function(response) {
+                    return response.json();
+                })
+                .then(function(data) {
 
-            location.innerHTML = data.name;
-            temperature.innerHTML = data.main.temp;
-            description.innerHTML = data.weather[0].description;
-            console.log(data)
-        })
-        .catch(function() {
-            console.log('Error')
-        })}else{
-            document.getElementById('message').innerHTML = 'Invalid input'
+                    // Convert temperature from Kelvin to Celsius
+                    let tempInCelsius = data.main.temp - 273.15;
+
+                    location.innerHTML = data.name;
+                    temperature.innerHTML = tempInCelsius.toFixed(2) + ' °C'; // Display temperature in Celsius
+                    description.innerHTML = data.weather[0].description;
+                    console.log(data);
+                })
+                .catch(function() {
+                    console.log('Error');
+                });
+        } else {
+            document.getElementById('message').innerHTML = 'Invalid input';
         }
     }
 
-    //event listener for form submission
+    // Event listener for form submission
     form.addEventListener('submit', (e) => {
         e.preventDefault();
 
-        //invoking weather function upon submission
+        // Invoking weather function upon submission
         weather();
-        
-    })
-})
+    });
+});
